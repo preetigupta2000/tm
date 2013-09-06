f2b4191164c0730caa1ad0385cf0065ae10b0c60
preeti.gupta@comprotechnologies.com <preeti.gupta@comprotechnologies.com>
2013-09-06 20:58:19 +0530

    Adding Resgiatration controller after resolving conflicts.

---
+++ grails-app/controllers/com/fonantrix/tm/RegistrationController.groovy
@@ -0,0 +1,51 @@
+package com.fonantrix.tm 
+
+
+import com.fonantrix.tm.authenticate.Role
+import com.fonantrix.tm.authenticate.User
+import com.fonantrix.tm.authenticate.UserRole;
+
+import org.hibernate.HibernateException
+
+class RegistrationController  {
+
+	def springSecurityService
+	
+	/*
+	 GET	show
+	 PUT	update
+	 POST	save
+	 DELETE	delete
+	 */
+
+	
+    def index() {
+		render(view: "/registrationform")
+	}
+	
+	def save = {	
+		def testUser = new User(username: params.username, email: params.username, firstName:params.firstName,lastName:params.lastName,password: params.password)		
+		try {
+			testUser.save(failOnError: true)
+			//changes made by sunil	
+			//def userInfo= new UserInfo(designation:"designation",corresAddress:"address",user:testUser)
+			//userInfo.save(failOnError: true)  //changes made by sunil
+			List<Role> roleList = Role.findAllByAuthority("ROLE_USER")
+			Role userRole = roleList.get(0)
+			UserRole.create testUser, userRole, true
+			redirect action: 'index', controller: 'home'
+			return
+		}
+		catch(HibernateException e)
+		{
+			render (text:testUser.errors,status:500)
+			return
+		}
+		
+	}
+	
+	
+	
+}
+
+
