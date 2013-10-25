9809a924c402a6b326ea0daf718be993a5a0723c
preeti.gupta@comprotechnologies.com <preeti.gupta@comprotechnologies.com>
2013-10-25 22:48:30 +0530

    Adding time after resolving conflicts

---
+++ grails-app/domain/com/fonantrix/tm/Time.groovy
@@ -0,0 +1,27 @@
+package com.fonantrix.tm
+
+import com.fonantrix.tm.authenticate.User
+
+class Time {
+
+		String start
+		String end
+		String project
+		String task
+		User user   
+		String title
+		String actualHours
+		int estimatedHrs
+		static belongsTo = [task: Task]   
+		
+		static mapping = {
+			
+			user unique: false
+			cache usage:'read-write', include:'non-lazy'
+		}
+		static constraints = {
+			actualHours nullable: true
+			end nullable: true
+
+		}
+}
